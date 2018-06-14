660d82c521744188f2827fb6b602a63fc1c5fc14
Borys <bogori92@gmail.com>
2018-06-15 00:43:02 +0300

    fix background for search bar; delete Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Clock from './components/clock';
-import SearchBar from './components/searchbar'
-import Photos from './components/background/index';
+import React, { Component } from "react";
+import "./App.css";
+import Clock from "./components/clock";
+import SearchBar from "./components/searchbar";
+import Photos from "./components/background/index";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Photos />
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Clock />
         <SearchBar />
       </div>
