1b7243fc43f539031e6253f2c05d921456ff5b9e
Borys <bogori92@gmail.com>
2018-06-16 21:45:11 +0300

    add link component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Clock from "./components/clock";
 import SearchBar from "./components/searchbar";
 import Photos from "./components/background/index";
+import Links from "./components/quicklinks";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Links />
         <Photos />
         <Clock />
         <SearchBar />
