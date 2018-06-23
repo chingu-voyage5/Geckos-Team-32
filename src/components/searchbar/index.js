4f071bdb9d0b4d5f2a1612402dc6bc8fddd9a045
Borys <bogori92@gmail.com>
2018-06-23 18:30:29 +0300

    add autofocus when rendered

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -4,7 +4,7 @@ import "./searchbar.css";
 const SearchBar = () => {
   return (
     <form id="searchBar" action="https://www.google.com/search" method="get">
-      <input type="search" name="q" placeholder="Search..." />
+      <input type="search" name="q" placeholder="Search..." autoFocus />
       <input type="submit" value="Search" />
     </form>
   );
